19d831eba26dba86b922de9fd05de5adeaa9dac1
lengushuru <charleslengushuru@gmail.com>
2024-01-21 23:45:02 +0300

    update home page css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,9 @@
 import React from "react";
 import Subcomp from "../navbar/homeComponent/Subcomp";
 import "./Home.css";
+import {data} from './homedata.js'
 const Home = () => {
-  const data = [{title:'Give Donation',
-    description:'You can register to donate monthly or one time donation to the CBO',
-    btn:'donate now'},
-    {title:'become a volunteer',
-    description:'You can register to voluter your time and skills to help the children',},
-    {title:'give scollarship',
-    description:'You can give scollaship to some of the students supported by the CBO'}
-  ]
+ 
   return (
     
     <section className="home">
---
+++ src/components/home/homedata.js
@@ -0,0 +1,8 @@
+export const data = [{title:'Give Donation',
+description:'You can register to donate monthly or one time donation to the CBO',
+btn:'donate now'},
+{title:'become a volunteer',
+description:'You can register to voluter your time and skills to help the children',},
+{title:'give scollarship',
+description:'You can give scollaship to some of the students supported by the CBO'}
+]
